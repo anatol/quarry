53cdbee15a586599fc439501dd31a3d3bebc1cd1
Anatol Pomozov <anatol.pomozov@gmail.com>
2014-06-12 09:20:11 -0700

    Allow to force rebuild packages
    Quarry rebuilds all the packages listed in file $ROOT/rebuild_packages

--- lib/quarry.rb
+++ lib/quarry.rb
@@ -158,10 +158,12 @@ def arch_to_pkg(arch_name)
   return [name, slot]
 end
 
-def load_whitelist_packages()
+def load_packages(packages_file, check_existance=true)
   result = []
-  # TOTHINK: load from config.yaml?
-  for l in IO.readlines(File.join(QUARRY_DIR, 'whitelist_packages'))
+  filename = File.join(QUARRY_DIR, packages_file)
+  return result unless File.exists?(filename)
+
+  for l in IO.readlines(filename)
     # format either 'package' or 'package,slot'
     pkg = l.strip.split(',')
     pkg << nil if pkg.size == 1
@@ -484,12 +486,13 @@ end
 init()
 
 existing_packages = load_arch_packages()
-whitelist_packages = load_whitelist_packages()
+whitelist_packages = load_packages('whitelist_packages')
 outdated_packages = out_of_date_packages(existing_packages)
 changed_dep_packages = package_with_changed_dependencies(existing_packages, outdated_packages)
+force_rebuild_packages = load_packages('rebuild_packages', false)
 
 # check if new packages appeared in the 'whitelist_packages' list
-packages_to_generate = whitelist_packages - existing_packages.keys + outdated_packages + changed_dep_packages
+packages_to_generate = whitelist_packages - existing_packages.keys + outdated_packages + changed_dep_packages + force_rebuild_packages
 
 packages_to_generate.uniq!
 
